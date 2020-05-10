cba7b4e7334402802900541c44078d7e5f9a4b0a
eclair6632 <masatoshi.moteki@gmail.com>
2020-05-10 20:28:52 +0900

    create_incremental-search-function_revision1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,18 @@
 class UsersController < ApplicationController
 
-def edit
+def index
+  return nil if params[:keyword] == ""
+  @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
+  respond_to do |format|
+    format.html
+    format.json
+  end
 end
 
-def update
+  def edit
+  end
+
+  def update
     if current_user.update(user_params)
       redirect_to root_path
     else
@@ -12,7 +21,6 @@ def update
   end
 
   private
-
   def user_params
     params.require(:user).permit(:name, :email)
   end
